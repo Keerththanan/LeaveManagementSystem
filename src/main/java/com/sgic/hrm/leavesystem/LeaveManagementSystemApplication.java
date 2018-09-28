7544afac605dc25adc76720c8cc17f4e6c44129b
unknown <kerthy92@yahoo.com>
2018-09-28 19:41:35 +0530

    Database entities successfully created with related relations

---
+++ src/main/java/com/sgic/hrm/leavesystem/LeaveManagementSystemApplication.java
@@ -0,0 +1,12 @@
+package com.sgic.hrm.leavesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaveManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaveManagementSystemApplication.class, args);
+	}
+}
